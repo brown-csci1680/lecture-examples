b002b31f89802bb3b222b14921ae72ab5e66eb35
Nicholas DeMarinis <ndemarinis@wpi.edu>
2022-01-31 17:26:52 -0500

    Initial test client and server.

---
+++ sockets-demo/utils.h
@@ -0,0 +1,14 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+void fatal_error(const char *message);
+
+void *get_in_addr(struct sockaddr *sa);
+
+#endif
