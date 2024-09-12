1fc2594101221768d80f4a54bbcb01bce2694706
Nicholas DeMarinis <ndemarinis@wpi.edu>
2024-09-12 08:06:14 -0400

    Added guessing game v0.

--- golang-sockets-init/guessing-game-v0/cmd/client/main.go
+++ golang-sockets-init/guessing-game-v0/cmd/client/main.go
@@ -3,80 +3,54 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"net"
 	"os"
-	"sockets-demo/pkg/game"
-	"sockets-demo/pkg/protocol"
 	"strconv"
 )
 
 func main() {
-	// Would like to write
-	//send("Hello world!")
-
-	// Dial now does two things
-	// - Creates a TCP socket
-	// - Establishes connection to server (connect() syscall)
-	conn, err := net.Dial("tcp4", "127.0.0.1:6666")
-	if err != nil {
-		// Abort program with stack trace (good enough for now)
-		panic(err)
-	}
+	//conn, err := net.Dial("tcp4", "127.0.0.1:6666")
+	// if err != nil {
+	// 	// Abort program with stack trace (good enough for now)
+	// 	panic(err)
+	// }
 
 	// Continuously read lines from stdin to send as guesses
 	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Enter a guess: ")
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		guess, err := strconv.Atoi(line)
 		if err != nil {
 			fmt.Println("Invalid input")
+			fmt.Printf("Enter a guess: ")
 			continue
 		}
 
-		// Build the guess message
-		guessMsg := protocol.GuessMessage{
-			MessageType: protocol.MessageTypeGuess,
-			Number:      int32(guess),
-		}
+		fmt.Println("User guessed ", guess)
 
-		// Turn it into a byte array
-		toSend, err := guessMsg.Marshal()
-		if err != nil {
-			panic(err)
-		}
+		//b, err := conn.Write(...)
 
-		// Send it
-		b, err := conn.Write(toSend)
-		if err != nil {
-			panic(err) // Good enough for now
-		}
+		// fmt.Printf("Sent %d bytes\n", b)
 
-		// Wait for a response
-		resp, err := protocol.ReadGuessMessage(conn)
-		if err != nil {
-			panic(err)
-		}
-
-		PrintResponses(resp)
-
-		fmt.Printf("Sent %d bytes\n", b)
+		// For next loop
+		fmt.Printf("Enter a guess: ")
 	}
 }
 
-func PrintResponses(msg *protocol.GuessMessage) {
-	if msg.MessageType == protocol.MessageTypeResponse {
-		switch msg.Number {
-		case game.GuessTooHigh:
-			fmt.Println("Too high!")
-		case game.GuessTooLow:
-			fmt.Println("Too low!")
-		case game.GuessCorrect:
-			fmt.Println("YAY!")
-		default:
-			fmt.Println("Invalid response:  ", msg.Number)
-		}
-	} else {
-		fmt.Println("Invalid message type:  ", msg.MessageType)
-	}
-}
+// func PrintResponses(msg *protocol.GuessMessage) {
+// 	if msg.MessageType == protocol.MessageTypeResponse {
+// 		switch msg.Number {
+// 		case game.GuessTooHigh:
+// 			fmt.Println("Too high!")
+// 		case game.GuessTooLow:
+// 			fmt.Println("Too low!")
+// 		case game.GuessCorrect:
+// 			fmt.Println("YAY!")
+// 		default:
+// 			fmt.Println("Invalid response:  ", msg.Number)
+// 		}
+// 	} else {
+// 		fmt.Println("Invalid message type:  ", msg.MessageType)
+// 	}
+// }
--- golang-sockets-init/guessing-game-v0/pkg/game/game.go
+++ golang-sockets-init/guessing-game-v0/pkg/game/game.go
@@ -1,59 +1,79 @@
 package game
 
-import (
-	"fmt"
-	"math/rand"
-	"sync"
-)
-
-// Warning:  fields here are shared data since they're
-// accessed by multiple clients!  Can protect with a mutex.
-//
-// Note:  struct fields starting with lower-case letters are private
-// This might be useful here since we want to make sure other parts
-// of the code can't read the fields without the mutex
-type GameInfo struct {
-	targetNumber int32
-	totalGuesses int
-
-	gameLock sync.Mutex
-}
-
 const (
 	GuessTooHigh = 1
 	GuessCorrect = 0
 	GuessTooLow  = -1
 )
 
-func InitializeGame() *GameInfo {
-	gi := &GameInfo{
-		// Fields initialized to zero if not specified
-	}
-	gi.resetGame()
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
-	return gi
-}
+// type GameInfo struct {
+// 	targetNumber int32 // Lower case fields are private, upper-case is public
+// 	totalGuesses int
+// }
 
-func (g *GameInfo) DoGuess(n int32) int32 {
-	g.gameLock.Lock()
-	defer g.gameLock.Unlock() // Defer runs this when the function returns
+// func InitializeGame() *GameInfo {
+// 	gi := &GameInfo{
+// 		// Fields initialized to zero if not specified
+// 	}
+// 	gi.resetGame()
 
-	g.totalGuesses++
+// 	return gi
+// }
 
-	if n < g.targetNumber {
-		return GuessTooLow
-	} else if n > g.targetNumber {
-		return GuessTooHigh
-	} else {
-		g.resetGame()
-		return GuessCorrect
-	}
-}
+// func (g *GameInfo) DoGuess(n int32) int32 {
+// 	g.totalGuesses++
 
-func (g *GameInfo) resetGame() {
-	// Should only be called when lock is held
-	g.targetNumber = rand.Int31n(8192)
-	g.totalGuesses = 0
+// 	if n < g.targetNumber {
+// 		return GuessTooLow
+// 	} else if n > g.targetNumber {
+// 		return GuessTooHigh
+// 	} else {
+// 		g.resetGame()
+// 		return GuessCorrect
+// 	}
+// }
 
-	fmt.Println("Target number is:  ", g.targetNumber)
-}
+// func (g *GameInfo) resetGame() {
+// 	// Should only be called when lock is held
+// 	g.targetNumber = rand.Int31n(8192)
+// 	g.totalGuesses = 0
+
+// 	fmt.Println("Target number is:  ", g.targetNumber)
+// }
